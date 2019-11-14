1515189d5aaada40d815f81cc4f0ec8df779704d
shunf4 <shun1048576@gmail.com>
2019-11-14 19:16:17 +0800

    fix: type error about tchar

--- setproxy/setproxy.cpp
+++ setproxy/setproxy.cpp
@@ -33,9 +33,9 @@ void LogString(const TCHAR *lpsz, ...)
 	va_start(list, lpsz);
 	HRESULT hr = StringCchVPrintf(szBuffer, ARRAYSIZE(szBuffer), lpsz, list);
 
-	if (SUCCEEDED(hr) && strlen(szBuffer))
+	if (SUCCEEDED(hr) && _tcslen(szBuffer))
 	{
-		printf("%s\r\n", szBuffer);	
+		printf("%ws\r\n", szBuffer);	
 	}
 	else
 	{
@@ -48,7 +48,7 @@ void LogString(const TCHAR *lpsz, ...)
 void NotifyProxyChanged()
 {
 	//To alert all available WinInet instances, set the Buffer parameter of InternetSetOption to NULL and BufferLength to 0 when passing this option
-	LogString("Calling InternetSetOption with NULL handle and option INTERNET_OPTION_PROXY_SETTINGS_CHANGED");
+	LogString(_TEXT("Calling InternetSetOption with NULL handle and option INTERNET_OPTION_PROXY_SETTINGS_CHANGED"));
 	InternetSetOption(NULL, INTERNET_OPTION_PROXY_SETTINGS_CHANGED, NULL, 0);
 }
 
@@ -139,7 +139,7 @@ BOOL DumpPerConnOption(INTERNET_PER_CONN_OPTION &Option)
 			printf("\n(%d)INTERNET_PER_CONN_PROXY_SERVER\n", Option.dwOption);
 			if (Option.Value.pszValue != NULL)
 			{
-				printf("\tProxy server : %s\n", Option.Value.pszValue);
+				printf("\tProxy server : %ws\n", Option.Value.pszValue);
 				GlobalFree(Option.Value.pszValue);
 			}
 		}
@@ -151,7 +151,7 @@ BOOL DumpPerConnOption(INTERNET_PER_CONN_OPTION &Option)
 			//Sets or retrieves a string containing the URLs that do not use the proxy server. 
 			if (Option.Value.pszValue != NULL)
 			{
-				printf("\tProxy bypass list :%s\n", Option.Value.pszValue);
+				printf("\tProxy bypass list :%ws\n", Option.Value.pszValue);
 				GlobalFree(Option.Value.pszValue);
 			}
 		}
@@ -163,7 +163,7 @@ BOOL DumpPerConnOption(INTERNET_PER_CONN_OPTION &Option)
 			//Sets or retrieves a string containing the URL to the automatic configuration script. 
 			if (Option.Value.pszValue != NULL)
 			{
-				printf("\tAutoConfig url : %s\n", Option.Value.pszValue);
+				printf("\tAutoConfig url : %ws\n", Option.Value.pszValue);
 				GlobalFree(Option.Value.pszValue);
 			}
 		}
@@ -283,7 +283,7 @@ BOOL GetProxySettings()
 	List.dwOptionError = 0;
 	List.pOptions = Option;
 
-	LogString("Calling InternetQueryOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION and 10 options");
+	LogString(_TEXT("Calling InternetQueryOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION and 10 options"));
 	if (!InternetQueryOption(NULL, INTERNET_OPTION_PER_CONNECTION_OPTION, &List, &nSize))
 	{
 		printf("InternetQueryOption failed! (%d)\n", GetLastError());
@@ -293,7 +293,7 @@ BOOL GetProxySettings()
 	/*INTERNET_VERSION_INFO      Version;
 	nSize = sizeof(INTERNET_VERSION_INFO);
 
-	LogString("Calling InternetQueryOption with NULL handle and option INTERNET_OPTION_VERSION");
+	LogString(_TEXT("Calling InternetQueryOption with NULL handle and option INTERNET_OPTION_VERSION"));
 	InternetQueryOption(NULL, INTERNET_OPTION_VERSION, &Version, &nSize);
 	printf("Version : %d.%d\n", Version.dwMajorVersion, Version.dwMinorVersion);*/
 
@@ -317,16 +317,16 @@ BOOL ConfigureProxy(SETPROXY_ACTION action)
 
 	if (ConnectionName != NULL)
 	{
-		LogString("Currrent proxy settings for Connection : %s\n", ConnectionName);
+		LogString(_TEXT("Currrent proxy settings for Connection : %s\n"), ConnectionName);
 	}
 	else
 	{
-		LogString("Currrent proxy settings for Lan Settings\n");
+		LogString(_TEXT("Currrent proxy settings for Lan Settings\n"));
 	}
 
-	LogString("***********************\n");
+	LogString(_TEXT("***********************\n"));
 	GetProxySettings();
-	LogString("\n***********************\n");
+	LogString(_TEXT("\n***********************\n"));
 
 	/*
 	** First have to query for autodiscovery flags. They're used later in the
@@ -340,14 +340,14 @@ BOOL ConfigureProxy(SETPROXY_ACTION action)
 
 	Option[1].dwOption = INTERNET_PER_CONN_AUTODISCOVERY_FLAGS;
 	Option[0].dwOption = INTERNET_PER_CONN_FLAGS;
-	LogString("Calling InternetQueryOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION/INTERNET_PER_CONN_AUTODISCOVERY_FLAGS and INTERNET_PER_CONN_FLAGS");
+	LogString(_TEXT("Calling InternetQueryOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION/INTERNET_PER_CONN_AUTODISCOVERY_FLAGS and INTERNET_PER_CONN_FLAGS"));
 	if (FALSE != InternetQueryOption(
 		NULL,
 		INTERNET_OPTION_PER_CONNECTION_OPTION,
 		&List,
 		&cbList))
 	{
-		LogString("InternetQueryOption succeeded");
+		LogString(_TEXT("InternetQueryOption succeeded"));
 
 		/*
 		** InetCPL sets PROXY_TYPE_DIRECT regardless of the state of the
@@ -370,43 +370,43 @@ BOOL ConfigureProxy(SETPROXY_ACTION action)
 
 		//Should be in the retrieved option
 		//Option[1].Value.dwValue = PROXY_TYPE_DIRECT;
-		//LogString("Option:INTERNET_PER_CONN_FLAGS Value:PROXY_TYPE_DIRECT");
+		//LogString(_TEXT("Option:INTERNET_PER_CONN_FLAGS Value:PROXY_TYPE_DIRECT"));
 		if (AUTO == action)
 		{
 			Option[0].Value.dwValue |= PROXY_TYPE_AUTO_DETECT;
-			LogString("INTERNET_PER_CONN_FLAGS Value |= PROXY_TYPE_AUTO_DETECT");
+			LogString(_TEXT("INTERNET_PER_CONN_FLAGS Value |= PROXY_TYPE_AUTO_DETECT"));
 			Option[1].Value.dwValue |= AUTO_PROXY_FLAG_USER_SET;
-			LogString("INTERNET_PER_CONN_AUTODISCOVERY_FLAGS Value |= AUTO_PROXY_FLAG_USER_SET");
+			LogString(_TEXT("INTERNET_PER_CONN_AUTODISCOVERY_FLAGS Value |= AUTO_PROXY_FLAG_USER_SET"));
 		}
 		else
 		{
 			Option[1].Value.dwValue &= ~AUTO_PROXY_FLAG_DETECTION_RUN;
-			LogString("INTERNET_PER_CONN_AUTODISCOVERY_FLAGS Value &= ~AUTO_PROXY_FLAG_DETECTION_RUN");
+			LogString(_TEXT("INTERNET_PER_CONN_AUTODISCOVERY_FLAGS Value &= ~AUTO_PROXY_FLAG_DETECTION_RUN"));
 			if (MANUAL == action)
 			{
 				Option[0].Value.dwValue |= PROXY_TYPE_PROXY;
-				LogString("INTERNET_PER_CONN_FLAGS Value |= PROXY_TYPE_PROXY");
+				LogString(_TEXT("INTERNET_PER_CONN_FLAGS Value |= PROXY_TYPE_PROXY"));
 			}
 			else if (AUTOCONFIG == action)
 			{
 				Option[0].Value.dwValue |= PROXY_TYPE_AUTO_PROXY_URL;
-				LogString("INTERNET_PER_CONN_FLAGS Value |= PROXY_TYPE_AUTO_PROXY_URL");
+				LogString(_TEXT("INTERNET_PER_CONN_FLAGS Value |= PROXY_TYPE_AUTO_PROXY_URL"));
 			}
 			else if (action == RESET)
 			{
 				Option[0].Value.dwValue = 0;
-				LogString("INTERNET_PER_CONN_FLAGS Value set to zero");
+				LogString(_TEXT("INTERNET_PER_CONN_FLAGS Value set to zero"));
 				Option[1].Value.dwValue &= ~AUTO_PROXY_FLAG_USER_SET;
-				LogString("INTERNET_PER_CONN_AUTODISCOVERY_FLAGS Value &= ~AUTO_PROXY_FLAG_USER_SET");
+				LogString(_TEXT("INTERNET_PER_CONN_AUTODISCOVERY_FLAGS Value &= ~AUTO_PROXY_FLAG_USER_SET"));
 			}
 			else if (action == DIRECT)
 			{
 				Option[0].Value.dwValue |= PROXY_TYPE_DIRECT;
-				LogString("INTERNET_PER_CONN_FLAGS |= PROXY_TYPE_DIRECT");
+				LogString(_TEXT("INTERNET_PER_CONN_FLAGS |= PROXY_TYPE_DIRECT"));
 			}
 		}
 
-		LogString("Calling InternetSetOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION");
+		LogString(_TEXT("Calling InternetSetOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION"));
 
 		if (InternetSetOption(NULL, INTERNET_OPTION_PER_CONNECTION_OPTION, &List, cbList))
 		{
@@ -416,12 +416,12 @@ BOOL ConfigureProxy(SETPROXY_ACTION action)
 		}
 		else
 		{
-			LogString("InternetSetOption failed for ConfigureProxy! (%d)\n", GetLastError());
+			LogString(_TEXT("InternetSetOption failed for ConfigureProxy! (%d)\n"), GetLastError());
 		}
 	}
 	else
 	{
-		LogString("InternetQueryOption failed for ConfigureProxy! (%d)\n", GetLastError());
+		LogString(_TEXT("InternetQueryOption failed for ConfigureProxy! (%d)\n"), GetLastError());
 	}
 	return bReturn;
 }
@@ -443,7 +443,7 @@ BOOL SetProxyOption(__in_opt INT iProxyOption, __in_opt TCHAR * pszValue)
 	List.dwOptionError = 0;
 	List.pOptions = Option;
 
-	LogString("Calling InternetSetOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION with option %d/%X value : %s", iProxyOption, iProxyOption, pszValue);
+	LogString(_TEXT("Calling InternetSetOption with NULL handle and option INTERNET_OPTION_PER_CONNECTION_OPTION with option %d/%X value : %s"), iProxyOption, iProxyOption, pszValue);
 	if (InternetSetOption(NULL, INTERNET_OPTION_PER_CONNECTION_OPTION, &List, nSize))
 	{
 		NotifyProxyChanged();
@@ -451,7 +451,7 @@ BOOL SetProxyOption(__in_opt INT iProxyOption, __in_opt TCHAR * pszValue)
 	}
 	else
 	{
-		LogString("InternetSetOption failed for SetProxyOption! (%d)\n", GetLastError());
+		LogString(_TEXT("InternetSetOption failed for SetProxyOption! (%d)\n"), GetLastError());
 	}
 	return bReturn;
 
@@ -492,7 +492,7 @@ void Usage()
 	printf("InternetQueryOption function https://msdn.microsoft.com/en-us/library/aa385101(v=vs.85).aspx\n");
 }
 
-INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
+INT __cdecl wmain(int argc, __in_ecount(argc) wchar_t **argv)
 {
 	INT iReturn = -1;
 
@@ -517,12 +517,12 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 	if (argc >1)
 	{
 		// AutoconfigURL should be before Auto since first 4 letters are the same 
-		if (0 == _strnicmp("autoconfigURL", argv[1], 13))
+		if (0 == _tcsnicmp(_TEXT("autoconfigURL"), argv[1], 13))
 		{
 			// Adding the Action before if (argc >2) statement since user might specify blank as the autoconfig 
 			// Incase user specifies blank in Inetcpl.cpl then the connection is returned to Direct. We are simulating this behaviour			
 			action = AUTOCONFIG;
-			LogString("Setting AutoConfig url");
+			LogString(_TEXT("Setting AutoConfig url"));
 			if (argc >2)
 			{
 				pszBuffer = argv[2];
@@ -533,26 +533,26 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 			}
 			else
 			{
-				pszBuffer = "";
+				pszBuffer = _TEXT("");
 			}
 
 		}
-		else if (0 == _strnicmp("auto", argv[1], 4))
+		else if (0 == _tcsnicmp(_TEXT("auto"), argv[1], 4))
 		{
 			action = AUTO;
-			LogString("Setting Auto detect proxy settings.");
+			LogString(_TEXT("Setting Auto detect proxy settings."));
 			if (argc > 2)
 			{
 				ConnectionName = argv[2];
 			}
 		}
-		else if (0 == _strnicmp("manual", argv[1], 6))
+		else if (0 == _tcsnicmp(_TEXT("manual"), argv[1], 6))
 		{
 			action = MANUAL;
 			if (argc >2)
 			{
 				action = MANUALSERVER;
-				LogString("Setting Hardcoded proxy");
+				LogString(_TEXT("Setting Hardcoded proxy"));
 				pszBuffer = argv[2];
 			}
 			if (argc > 3)
@@ -560,21 +560,21 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 				ConnectionName = argv[3];
 			}
 		}
-		else if (0 == _strnicmp("direct", argv[1], 6))
+		else if (0 == _tcsnicmp(_TEXT("direct"), argv[1], 6))
 		{
 			action = DIRECT;
-			LogString("Setting Direct Internet Access, proxy disabled.");
+			LogString(_TEXT("Setting Direct Internet Access, proxy disabled."));
 			if (argc > 2)
 			{
 				ConnectionName = argv[2];
 			}
 		}
-		else if (0 == _strnicmp("bypass", argv[1], 6))
+		else if (0 == _tcsnicmp(_TEXT("bypass"), argv[1], 6))
 		{
 			if (argc >2)
 			{
 				action = BYPASS;
-				LogString("Setting the Proxy Bypass addresses");
+				LogString(_TEXT("Setting the Proxy Bypass addresses"));
 				pszBuffer = argv[2];
 			}
 			if (argc > 3)
@@ -582,16 +582,16 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 				ConnectionName = argv[3];
 			}
 		}
-		else if (0 == _strnicmp("reset", argv[1], 5))
+		else if (0 == _tcsnicmp(_TEXT("reset"), argv[1], 5))
 		{
 			action = RESET;
-			LogString("Resetting the proxy settings");
+			LogString(_TEXT("Resetting the proxy settings"));
 			if (argc > 2)
 			{
 				ConnectionName = argv[2];
 			}
 		}
-		else if (0 == _strnicmp("show", argv[1], 5))
+		else if (0 == _tcsnicmp(_TEXT("show"), argv[1], 5))
 		{
 			action = SHOW;
 			if (argc > 2)
@@ -610,8 +610,8 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 
 	if (ConnectionName != NULL)
 	{
-		LogString("Warning : if connection name  %s does not exits , it will be created with LAN Settings!\n", ConnectionName);
-		LogString("Warning if connection name contains non ASCII character (accentuated), the created entry will not be correct on Windows Creators Update\n");
+		LogString(_TEXT("Warning : if connection name %ls does not exits , it will be created with LAN Settings!\n"), ConnectionName);
+		LogString(_TEXT("Warning if connection name contains non ASCII character (accentuated), the created entry will not be correct on Windows Creators Update\n"));
 	}
 
 	switch (action)
@@ -619,34 +619,34 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 	case AUTO:
 		if (TRUE == ConfigureProxy(AUTO))
 		{
-			LogString("Successfully configured WinINet to use the Automatic Proxy detection.");
+			LogString(_TEXT("Successfully configured WinINet to use the Automatic Proxy detection."));
 			iReturn = 0;
 		}
 		else
 		{
-			LogString("Failed to configure the Automatic Proxy detection.");
+			LogString(_TEXT("Failed to configure the Automatic Proxy detection."));
 		}
 		break;
 	case RESET:
 		if (TRUE == ConfigureProxy(RESET))
 		{
-			LogString("Successfully reset of WinINet proxy settings (including removing DIRECT!)");
+			LogString(_TEXT("Successfully reset of WinINet proxy settings (including removing DIRECT!)"));
 			iReturn = 0;
 		}
 		else
 		{
-			LogString("Failed to reset WinINet proxy settings.");
+			LogString(_TEXT("Failed to reset WinINet proxy settings."));
 		}
 		break;
 	case MANUAL:
 		if (TRUE == ConfigureProxy(MANUAL))
 		{
-			LogString("Successfully configured WinINet to use Manual Proxy settings.");
+			LogString(_TEXT("Successfully configured WinINet to use Manual Proxy settings."));
 			iReturn = 0;
 		}
 		else
 		{
-			LogString("Failed to configure the Proxy settings to Manual.");
+			LogString(_TEXT("Failed to configure the Proxy settings to Manual."));
 		}
 		break;
 
@@ -655,39 +655,39 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 		{
 			if (TRUE == SetProxyServer(pszBuffer))
 			{
-				LogString("Successfully configured WinINet Manual Proxy server to %S.", pszBuffer);
+				LogString(_TEXT("Successfully configured WinINet Manual Proxy server to %S."), pszBuffer);
 				iReturn = 0;
 			}
 			else
 			{
-				LogString("Failed to configure the Manual Proxy server using %S.", pszBuffer);
+				LogString(_TEXT("Failed to configure the Manual Proxy server using %S."), pszBuffer);
 			}
 		}
 		else
 		{
-			LogString("Failed to configure the Proxy settings to Manual.");
+			LogString(_TEXT("Failed to configure the Proxy settings to Manual."));
 		}
 		break;
 	case DIRECT:
 		if (TRUE == ConfigureProxy(DIRECT))
 		{
-			LogString("Successfully configured WinINet to use Direct Internet access. The Proxy setting have been turned off.");
+			LogString(_TEXT("Successfully configured WinINet to use Direct Internet access. The Proxy setting have been turned off."));
 			iReturn = 0;
 		}
 		else
 		{
-			LogString("Failed to configure the Proxy setting for Direct Internet Access.");
+			LogString(_TEXT("Failed to configure the Proxy setting for Direct Internet Access."));
 		}
 		break;
 	case BYPASS:
 		if (TRUE == SetProxyByPass(pszBuffer))
 		{
-			LogString("Successfully configured WinINet Proxy ByPass settings to %S.", pszBuffer);
+			LogString(_TEXT("Successfully configured WinINet Proxy ByPass settings to %S."), pszBuffer);
 			iReturn = 0;
 		}
 		else
 		{
-			LogString("Failed to configure the Proxy ByPass settings.");
+			LogString(_TEXT("Failed to configure the Proxy ByPass settings."));
 		}
 		break;
 	case AUTOCONFIG:
@@ -696,31 +696,31 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 
 			if (TRUE == SetProxyAutoConfig(pszBuffer))
 			{
-				LogString("Successfully configured WinINet AutoconfigURL to %S.", pszBuffer);
+				LogString(_TEXT("Successfully configured WinINet AutoconfigURL to %S."), pszBuffer);
 				iReturn = 0;
 			}
 			else
 			{
-				LogString("Failed to configure IE to AutoconfigURL using %S.", pszBuffer);
+				LogString(_TEXT("Failed to configure IE to AutoconfigURL using %S."), pszBuffer);
 			}
 		}
 		else
 		{
-			LogString("Failed to configure the Proxy settings to Manual.");
+			LogString(_TEXT("Failed to configure the Proxy settings to Manual."));
 		}
 		break;
 	case SHOW:
 		if (ConnectionName != NULL)
 		{
-			LogString("Currrent proxy settings for Connection : %s\n", ConnectionName);
+			LogString(_TEXT("Currrent proxy settings for Connection : %s\n"), ConnectionName);
 		}
 		else
 		{
-			LogString("Currrent proxy settings for Lan Settings\n");
+			LogString(_TEXT("Currrent proxy settings for Lan Settings\n"));
 		}
-		LogString("***********************\n");
+		LogString(_TEXT("***********************\n"));
 		GetProxySettings();
-		LogString("\n***********************\n");
+		LogString(_TEXT("\n***********************\n"));
 		return 0L;
 	default:
 		Usage();
@@ -729,15 +729,15 @@ INT __cdecl main(int argc, __in_ecount(argc) LPSTR  *argv)
 
 	if (ConnectionName != NULL)
 	{
-		LogString("New proxy settings for Connection : %s\n", ConnectionName);
+		LogString(_TEXT("New proxy settings for Connection : %s\n"), ConnectionName);
 	}
 	else
 	{
-		LogString("New proxy settings for Lan Settings\n");
+		LogString(_TEXT("New proxy settings for Lan Settings\n"));
 	}
-	LogString("***********************\n");
+	LogString(_TEXT("***********************\n"));
 	GetProxySettings();
-	LogString("\n***********************\n");
+	LogString(_TEXT("\n***********************\n"));
 
 	if (SUCCEEDED(hrCoInit))
 	{
